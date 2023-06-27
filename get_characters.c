2afff41bb82ed4caef29606debd884a75bba30d9
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-27 16:35:11 +0100

    Update get_characters.c

--- get_characters.c
+++ get_characters.c
@@ -1,12 +1,12 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * print_char - Prints a single character
+ * print_characters - Prints a single character
  * @args: Variable argument list
  *
  * Return: Number of characters printed (always 1)
  */
-int print_char(va_list args)
+int print_characters(va_list args)
 {
 	int ch = va_arg(args, int);
 

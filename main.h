434e3843dceb6fe01564b9183cbef6f7293f309b
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-27 16:57:57 +0100

    Update main.h

--- main.h
+++ main.h
@@ -6,17 +6,17 @@
 /* Struct to hold format and variable arguments */
 typedef struct printf_data
 {
-    const char *format;
-    va_list arguments;
-    int count;
+	const char *format;
+	va_list arguments;
+	int count;
 } printf_data;
 
 /* Function prototypes */
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list arguments);
+int print_characters(va_list arguments);
 int print_string(va_list arguments);
 int print_percent(va_list arguments);
-int print_digit(va_list arguments);
+int print_decimal(va_list arguments);
 
 #endif /* MAIN_H */

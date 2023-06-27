63e78ad8c710e6d397be648b5b055d2062115ce9
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-27 14:25:56 +0100

    Update 1-printf.c

--- 1-printf.c
+++ 1-printf.c
@@ -1,50 +1,61 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-typedef struct {
+typedef struct printf_data
+{
     const char *format;
     va_list args;
     int count;
 } printf_data;
 
 typedef int (*printf_handler)(printf_data *);
-
-int print_char(printf_data *data) {
+int print_char(printf_data *data)
+{
     int ch = va_arg(data->args, int);
     putchar(ch);
     data->count++;
     return 0;
 }
-
-int print_string(printf_data *data) {
+int print_string(printf_data *data)
+{
     char *str = va_arg(data->args, char *);
     int len = 0;
-    while (*str != '\0') {
+    while (*str != '\0')
+    {
         putchar(*str++);
         len++;
     }
     data->count += len;
-    return len;
+    return (len);
 }
-
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
     printf_data data;
     data.format = format;
     va_start(data.args, format);
     data.count = 0;
 
-    while (*data.format != '\0') {
-        if (*data.format == '%') {
+    while (*data.format != '\0')
+    {
+        if (*data.format == '%')
+        {
             data.format++;
-            if (*data.format == 'c') {
+            if (*data.format == 'c')
+            {
                 print_char(&data);
-            } else if (*data.format == 's') {
+            }
+            else if (*data.format == 's')
+            {
                 print_string(&data);
-            } else if (*data.format == '%') {
+            }
+            else if (*data.format == '%')
+            {
                 putchar('%');
                 data.count++;
             }
-        } else {
+        }
+        else
+        {
             putchar(*data.format);
             data.count++;
         }
@@ -52,5 +63,5 @@ int _printf(const char *format, ...) {
     }
 
     va_end(data.args);
-    return data.count;
+    return (data.count);
 }

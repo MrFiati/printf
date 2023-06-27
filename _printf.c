5ea6731409557e34711a45ce0fcf05f1490e0dca
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-27 12:08:35 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -2,12 +2,24 @@
 #include <stdarg.h>
 #include "main.h"
 
+/**
+ * print_char - Prints a single character
+ * @ch: The character to print
+ *
+ * Return: Number of characters printed (always 1)
+ */
 int print_char(char ch)
 {
 	putchar(ch);
 	return (1);
 }
 
+/**
+ * print_string - Prints a string
+ * @str: The string to print
+ *
+ * Return: Number of characters printed
+ */
 int print_string(char *str)
 {
 	int counter = 0;
@@ -21,6 +33,12 @@ int print_string(char *str)
 	return (counter);
 }
 
+/**
+ * _printf - Custom printf function implementation
+ * @format: Format string containing the text to be printed
+ *
+ * Return: Number of characters printed
+ */
 int _printf(const char *format, ...)
 {
 	const char *ptr = format;
@@ -61,5 +79,5 @@ int _printf(const char *format, ...)
 	}
 
 	va_end(arguments);
-	return counter;
+	return (counter);
 }

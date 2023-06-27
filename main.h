94d67dc214a5186e369cce5c084aa6f31a25c58c
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-27 16:52:54 +0100

    Update main.h

--- main.h
+++ main.h
@@ -7,16 +7,16 @@
 typedef struct printf_data
 {
     const char *format;
-    va_list args;
+    va_list arguments;
     int count;
 } printf_data;
 
 /* Function prototypes */
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list args);
-int print_string(va_list args);
-int print_percent(va_list args);
-int print_digit(va_list args);
+int print_char(va_list arguments);
+int print_string(va_list arguments);
+int print_percent(va_list arguments);
+int print_digit(va_list arguments);
 
 #endif /* MAIN_H */

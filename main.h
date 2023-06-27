861f14282c8b04c9faaf2c63d1ce2462680d5ff2
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-27 14:45:55 +0100

    Update main.h

--- main.h
+++ main.h
@@ -3,24 +3,11 @@
 
 #include <stdarg.h>
 #include <unistd.h>
-#include <stdio.h>
-#include <stdarg.h>
-/**
- * struct printf_data - Data structure for printf function
- * @format: The format string
- * @args: The va_list of arguments
- * @count: The count of characters printed
- */
-typedef struct printf_data
-{
-const char *format;
-va_list args;
-int count;
-} printf_data;
 
-int print_char(printf_data *data);
-int print_string(printf_data *data);
-int _printf(const char *format, ...);
 int _putchar(char c);
+int _printf(const char *format, ...);
+int print_numbers(int n);
+int print_char(char ch);
+int print_string(char *str);
 
 #endif

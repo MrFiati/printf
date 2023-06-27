fcce6dd6c500200eca71964287818763aa1cded2
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-27 16:45:36 +0100

    Update get_mod.c

--- get_mod.c
+++ get_mod.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 /**
  * print_percent - Prints a percent character
- * @args: Variable argument list
+ * @arguments: Variable argument list
  *
  * Return: Number of characters printed (always 1)
  */
 int print_percent(va_list args)
 {
-    (void)args; /* Unused parameter */
+    (void)arguments; 
     _putchar('%');
 
     return (1);

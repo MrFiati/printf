9a957ef5971ad4c4f69e48e15663ace4c246766d
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-27 14:20:18 +0100

    Update 1-printf.c

--- 1-printf.c
+++ 1-printf.c
@@ -1,84 +1,56 @@
 #include <stdio.h>
 #include <stdarg.h>
-#include "main.h"
 
-/**
- * print_char - Prints a single character
- * @ch: The character to print
- *
- * Return: Number of characters printed (always 1)
- */
-int print_char(char ch)
-{
-	_putchar(ch);
-	return (1);
+typedef struct {
+    const char *format;
+    va_list args;
+    int count;
+} printf_data;
+
+typedef int (*printf_handler)(printf_data *);
+
+int print_char(printf_data *data) {
+    int ch = va_arg(data->args, int);
+    putchar(ch);
+    data->count++;
+    return 0;
 }
 
-/**
- * print_string - Prints a string
- * @str: The string to print
- *
- * Return: Number of characters printed
- */
-int print_string(char *str)
-{
-	int counter = 0;
-
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		counter++;
-		str++;
-	}
-	return (counter);
+int print_string(printf_data *data) {
+    char *str = va_arg(data->args, char *);
+    int len = 0;
+    while (*str != '\0') {
+        putchar(*str++);
+        len++;
+    }
+    data->count += len;
+    return len;
 }
 
-/**
- * _printf - Custom printf function implementation
- * @format: Format string containing the text to be printed
- *
- * Return: Number of characters printed
- */
-int _printf(const char *format, ...)
-{
-	const char *ptr = format;
-	int counter = 0;
-	va_list arguments;
+int _printf(const char *format, ...) {
+    printf_data data;
+    data.format = format;
+    va_start(data.args, format);
+    data.count = 0;
 
-	va_start(arguments, format);
+    while (*data.format != '\0') {
+        if (*data.format == '%') {
+            data.format++;
+            if (*data.format == 'c') {
+                print_char(&data);
+            } else if (*data.format == 's') {
+                print_string(&data);
+            } else if (*data.format == '%') {
+                putchar('%');
+                data.count++;
+            }
+        } else {
+            putchar(*data.format);
+            data.count++;
+        }
+        data.format++;
+    }
 
-	for (ptr = format; *ptr != '\0'; ptr++)
-	{
-		if (*ptr == '%')
-		{
-			ptr++;
-			if (*ptr == 'c')
-			{
-				int ch = va_arg(arguments, int);
-				counter += print_char(ch);
-			}
-			else if (*ptr == 's')
-			{
-				char *str = va_arg(arguments, char *);
-				counter += print_string(str);
-			}
-			else if (*ptr == '%')
-			{
-				counter += print_char('%');
-			}
-			else
-			{
-				counter += print_char('%');
-				counter += print_char(*ptr);
-			}
-
-		}
-		else
-		{
-			counter += print_char(*ptr);
-		}
-	}
-
-	va_end(arguments);
-	return (counter);
+    va_end(data.args);
+    return data.count;
 }

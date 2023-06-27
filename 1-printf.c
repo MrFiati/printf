a2b919cfb8ed05392aee9d04968f643e8a324abc
Hamza Farissi <125490480+roxxe34@users.noreply.github.com>
2023-06-27 14:27:33 +0100

    Update 1-printf.c

--- 1-printf.c
+++ 1-printf.c
@@ -3,65 +3,62 @@
 
 typedef struct printf_data
 {
-    const char *format;
-    va_list args;
-    int count;
+const char *format;
+va_list args;
+int count;
 } printf_data;
-
 typedef int (*printf_handler)(printf_data *);
 int print_char(printf_data *data)
 {
-    int ch = va_arg(data->args, int);
-    putchar(ch);
-    data->count++;
-    return 0;
+int ch = va_arg(data->args, int);
+putchar(ch);
+data->count++;
+return (0);
 }
 int print_string(printf_data *data)
 {
-    char *str = va_arg(data->args, char *);
-    int len = 0;
-    while (*str != '\0')
-    {
-        putchar(*str++);
-        len++;
-    }
-    data->count += len;
-    return (len);
+char *str = va_arg(data->args, char *);
+int len = 0;
+while (*str != '\0')
+{
+_putchar(*str++);
+len++;
+}
+data->count += len;
+return (len);
 }
 int _printf(const char *format, ...)
 {
-    printf_data data;
-    data.format = format;
-    va_start(data.args, format);
-    data.count = 0;
-
-    while (*data.format != '\0')
-    {
-        if (*data.format == '%')
-        {
-            data.format++;
-            if (*data.format == 'c')
-            {
-                print_char(&data);
-            }
-            else if (*data.format == 's')
-            {
-                print_string(&data);
-            }
-            else if (*data.format == '%')
-            {
-                putchar('%');
-                data.count++;
-            }
-        }
-        else
-        {
-            putchar(*data.format);
-            data.count++;
-        }
-        data.format++;
-    }
-
-    va_end(data.args);
-    return (data.count);
+printf_data data;
+data.format = format;
+va_start(data.args, format);
+data.count = 0;
+while (*data.format != '\0')
+{
+if (*data.format == '%')
+{
+data.format++;
+if (*data.format == 'c')
+{
+print_char(&data);
+}
+else if (*data.format == 's')
+{
+print_string(&data);
+}
+else if (*data.format == '%')
+{
+_putchar('%');
+data.count++;
+}
+}
+else
+{
+_putchar(*data.format);
+data.count++;
+}
+data.format++;
+}
+va_end(data.args);
+return (data.count);
 }
